8238e0324ef722392e0f612cc346dde69be9de53
SeppoK <seppo.karvinen@helsinki.fi>
2016-09-25 15:12:08 +0300

    Added a line break command.

--- epsg_finder.py
+++ epsg_finder.py
@@ -9,13 +9,13 @@ Usage: ./epsg_finder.py
 '''
 
 
-user_value = input("What is the EPSG value of the input shapefile? ")
+user_value = input("What is the EPSG value of the input shapefile? \n")
 
 if user_value == "4326":
-    print ("EPSG value 4326 corresponds to WGS84")
+    print ("EPSG value 4326 corresponds to WGS84.")
 elif user_value == "3067":
-    print("EPSG value 3067 corresponds to ETRS-TM35FIN")
+    print("EPSG value 3067 corresponds to ETRS-TM35FIN.")
 elif user_value == "2392":
-    print("EPSG value 2392 corresponds to KKJ / Finland Zone 2")
+    print("EPSG value 2392 corresponds to KKJ / Finland Zone 2.")
 else:
     print("There are so many spatial references... I don’t know them all, sorry.")
